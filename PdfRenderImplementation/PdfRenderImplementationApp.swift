5552f7d6f1476be07585f4d9c9756a2349fe622e
Akintade Oluwaseun <akintadeseun816@gmail.com>
2025-11-17 03:46:17 +0000

    Initial Commit

---
+++ PdfRenderImplementation/PdfRenderImplementationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PdfRenderImplementationApp.swift
+//  PdfRenderImplementation
+//
+//  Created by BSTAR on 17/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct PdfRenderImplementationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
